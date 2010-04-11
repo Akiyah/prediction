9cf87ffe3b2ff5bcd41bf2284e841922470a49e6
Akiyah <akiya.mizukoshi@gmail.com>
2010-04-11 23:48:00 +0900

    point buy & payback

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,38 +1,20 @@
 class QuestionsController < ApplicationController
   # GET /questions
-  # GET /questions.xml
   def index
     @questions = Question.all
     @user = current_user
-
-#    respond_to do |format|
-#      format.html # index.html.erb
-#      format.xml  { render :xml => @questions }
-#    end
   end
 
   # GET /questions/1
-  # GET /questions/1.xml
   def show
     @question = Question.find(params[:id])
     @user = current_user
     @bet = current_bet(@user, @question)
-
-#    respond_to do |format|
-#      format.html # show.html.erb
-#      format.xml  { render :xml => @question }
-#    end
   end
 
   # GET /questions/new
-  # GET /questions/new.xml
   def new
     @question = Question.new
-
-#    respond_to do |format|
-#      format.html # new.html.erb
-#      format.xml  { render :xml => @question }
-#    end
   end
 
   # GET /questions/1/edit
@@ -67,37 +49,34 @@ class QuestionsController < ApplicationController
     @bet = current_bet(@user, @question)
 
     if @bet
+      @user.point += @bet.point
       @bet.destroy
       @bet = nil
     else
       @bet = Bet.create
-      @bet.point = params[:bet_point]
+      bet_point = params[:bet_point].to_i
+      @user.point -= bet_point
+      @bet.point = bet_point
       @bet.answer_index = params[:answer_index]
       @bet.user = @user
       @bet.question = @question
       @bet.save
     end
+    @user.save
 
     redirect_to(@question)
   end
 
   # DELETE /questions/1
-  # DELETE /questions/1.xml
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
 
     redirect_to(questions_url)
-#    respond_to do |format|
-#      format.html { redirect_to(questions_url) }
-#      format.xml  { head :ok }
-#    end
   end
 
   def current_bet(user, question)
     return nil if user == nil
-    p user
-    p question
     return Bet.find(:first,
                     :conditions=>["user_id = #{user.id} and question_id = #{question.id}"])
   end
